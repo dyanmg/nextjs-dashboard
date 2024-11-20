ababd27e49b83a94a2fd2b2292ea1f580caaea47
dyanmg <gufron.dian@gmail.com>
2024-11-20 21:52:20 +0700

    mutate complete

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,6 +1,9 @@
 'use server';
 
 import { z } from 'zod';
+import { sql } from '@vercel/postgres';
+import { expirePath } from 'next/cache';
+import { redirect } from 'next/navigation';
 
 const FormSchema = z.object({
     id: z.string(),
@@ -13,11 +16,19 @@ const FormSchema = z.object({
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
-    const rawFormData = {
+    const { customerId, amount, status } = CreateInvoice.parse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
-    }
+    });
+    const amountInCents = amount * 100;
+    const date = new Date().toISOString().split('T')[0];
 
-    console.log(rawFormData);
+    await sql`
+        INSERT INTO invoices (customer_id, amount, status, date)
+        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `;
+
+    expirePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
 }
