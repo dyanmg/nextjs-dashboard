354014d0ae0cf174eb9786139ce634f7a3ef58a2
dyanmg <gufron.dian@gmail.com>
2024-11-21 23:46:02 +0700

    mutating data & handling error

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -16,19 +16,62 @@ const FormSchema = z.object({
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
-    const { customerId, amount, status } = CreateInvoice.parse({
+    try {
+        const { customerId, amount, status } = CreateInvoice.parse({
+            customerId: formData.get('customerId'),
+            amount: formData.get('amount'),
+            status: formData.get('status'),
+        });
+        const amountInCents = amount * 100;
+        const date = new Date().toISOString().split('T')[0];
+    
+        await sql`
+            INSERT INTO invoices (customer_id, amount, status, date)
+            VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+        `;
+        expirePath('/dashboard/invoices');
+    } catch (error) {
+        return { message: 'Error creating invoice' };
+    }
+
+    redirect('/dashboard/invoices');
+}
+
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function updateInvoice(id: string, formData: FormData) {
+    const { customerId, amount, status } = UpdateInvoice.parse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
     });
-    const amountInCents = amount * 100;
-    const date = new Date().toISOString().split('T')[0];
 
-    await sql`
-        INSERT INTO invoices (customer_id, amount, status, date)
-        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-    `;
+    const amountInCents = amount * 100;
+
+    try {
+        await sql`
+            UPDATE invoices
+            SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+            WHERE id = ${id}
+        `;
+    } catch (error) {
+        return { message: 'Error updating invoice' };
+    }
 
     expirePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
+};
+
+export async function deleteInvoice(id: string) {
+    throw new Error('Not implemented');
+    try {
+        await sql`
+            DELETE FROM invoices
+            WHERE id = ${id}
+        `;
+        expirePath('/dashboard/invoices');
+        return { message: 'Invoice deleted' };
+    } catch (error) {
+        return { message: 'Error deleting invoice' };
+    }
 }
